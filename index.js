aa871e53919099d446113a3995a56131c5acf0b6
Geovane Saraiva da Silva <saraivasilva75@gmail.com>
2022-09-28 13:40:54 +0000

    Some changes

--- index.js
+++ index.js
@@ -6,19 +6,4 @@ const INDEX = '/index.html';
 
 app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(INDEX, { root: __dirname }));
-const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = require('socket.io')(server);
-
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
-    
-    socket.on('play-audio', function (src) {
-        io.emit('play-audio', src);
-    });
-    
-    socket.on('stop-audio', function () {
-        io.emit('stop-audio', new Date().toTimeString());
-    });
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
