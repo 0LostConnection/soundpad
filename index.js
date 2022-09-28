bf1f2ed6a859de5225606fc07c295f3ebfa6a4bd
Geovane Saraiva da Silva <saraivasilva75@gmail.com>
2022-09-28 14:58:37 +0000

    Some changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const INDEX = 'src/index.html';
 
 app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(INDEX, { root: __dirname }));
