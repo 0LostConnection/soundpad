d96dd3c15487e8c540c56dd575f52cd81b2a4075
Geovane Saraiva da Silva <saraivasilva75@gmail.com>
2023-10-10 17:19:38 -0300

    multi-clients version

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const INDEX = 'index.html';
+const INDEX = '/index.html';
 
 app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(INDEX, { root: __dirname }));
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+    
+    socket.on('play-audio', function (src) {
+        io.emit('play-audio', src);
+    });
+    
+    socket.on('stop-audio', function () {
+        io.emit('stop-audio', new Date().toTimeString());
+    });
+});
